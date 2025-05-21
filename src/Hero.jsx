e0e196acae50235d68aabf9eba25fc46f0d74e9d
vynas <hamdabousnina@gmail.com>
2025-05-20 20:12:11 -0400

    hero component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,17 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br />
+          Anywhere
+        </h1>
+        <p>
+          Strapi s the leading open-source headless CMS. it's 100% Javascript
+          and fully customizable
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Hero;
